eb6e049c9fa77d77d993c28ac3909872a3a70d29
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2021-08-21 15:21:00 +0500

    Added sliders

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,6 @@ let menuSpanOne = document.querySelector('.menu_span--one');
 let menuSpanTwo = document.querySelector('.menu_span--two');
 let menuSpanTop = document.querySelector('.menu_span--top');
 let menuSpanBottom = document.querySelector('.menu_span--bottom');
-let headerNav = document.querySelector('.header_nav');
 
 burger.addEventListener('click', () => {
   headerNav.classList.toggle('nav--active');
@@ -14,15 +13,57 @@ burger.addEventListener('click', () => {
   menuSpanTwo.classList.toggle('menu_span--two--active');
   menuSpanTop.classList.toggle('menu_span--top--active');
   menuSpanBottom.classList.toggle('menu_span--bottom--active');
-  headerNav.classList.toggle('nav--active');
 })
 
+// Slider best
+
+const swiper = new Swiper('.best_slider', {
+  initialSlide: 1,
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+
+
+  navigation: {
+    nextEl: '.best_button-next',
+    prevEl: '.best_button-prev',
+  },
+});
+// Slider feedback
+
+swiper = new Swiper('.feedback_slider', {
+  initialSlide: 1,
+
+  pagination: {
+    el: '.feedback_pagination ',
+    dynamicBullets: true,
+  },
+
+
+  navigation: {
+    nextEl: '.feedback_button-next',
+    prevEl: '.feedback_button-prev',
+  },
+});
 
 
 // accordeons
 
-// document.querySelectorAll('.accordion_item-trigger').forEach((item) =>
-//   item.addEventListener('click', () => {
-//     item.parentNode.classList.toggle('accordion_item--active');
-//   })
-// );
+document.querySelectorAll('.accordion_item-trigger').forEach((item) =>
+  item.addEventListener('click', () => {
+    const parent = item.parentNode;
+
+    if (parent.classList.contains('.accordion_item--active')) {
+      parent.classList.remove('.accordion_item--active');
+    } else {
+      document
+        .querySelectorAll('.accordion_item')
+        .forEach((child) => child.classList.remove('accordion_item--active'))
+
+      parent.classList.add('accordion_item--active')
+    }
+  })
+);
+
